aa83506959c12745f3d72360f3e7b1cfd2189150
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2022-08-09 14:35:09 +0100

    updated db

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ay7gIbHqt6R6yccqwCeC',
+        'HOST': 'containers-us-west-17.railway.app',
+        'PORT': '6185',
     }
 }
 
